195c479137423ce36527c271f5a1d630221c9821
jiun-dev <jiun.dev@gmail.com>
2021-05-11 21:02:19 +0900

    Initial Commit

---
+++ profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  profile
+//
+//  Created by shinjiun on 2021/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
